30ad4f05e363d1aa6b6e8a115f50d795112c60ef
Angela Aciobanitei <aciobanitei.angela@gmail.com>
2019-11-03 22:08:57 +0000

    Add draggable marker for choosing location

--- app/src/main/java/com/ang/acb/nasaapp/ui/earth/EarthMapFragment.java
+++ app/src/main/java/com/ang/acb/nasaapp/ui/earth/EarthMapFragment.java
@@ -16,11 +16,9 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.provider.Settings;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.ang.acb.nasaapp.BuildConfig;
 import com.ang.acb.nasaapp.R;
@@ -29,36 +27,38 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Locale;
-
 import dagger.android.support.AndroidSupportInjection;
 import timber.log.Timber;
 
-public class EarthMapFragment extends Fragment implements OnMapReadyCallback {
+public class EarthMapFragment extends Fragment implements
+        OnMapReadyCallback, LocationSource.OnLocationChangedListener {
 
-    // Used when checking for runtime permissions.
     private static final int LOCATION_PERMISSIONS_REQUEST_CODE = 123;
 
-    // Provides the entry point to the Fused Location Provider API.
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+    private static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
+    private static final LatLng DARWIN = new LatLng(-12.4634, 130.8456);
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+    private static final LatLng ADELAIDE = new LatLng(-34.92873, 138.59995);
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+    private static final LatLng ALICE_SPRINGS = new LatLng(-24.6980, 133.8807);
+
     private FusedLocationProviderClient fusedLocationClient;
-
-    // Represents a geographical location.
     private Location lastLocation;
-
     private GoogleMap map;
+    private double markerLat;
+    private double markerLng;
 
 
     // Required empty public constructor
@@ -73,13 +73,14 @@ public class EarthMapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment.
         return inflater.inflate(R.layout.fragment_earth_map, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         setupMapFragment();
         initLocationClient();
     }
@@ -109,7 +110,6 @@ public class EarthMapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Timber.d("onMapReady()");
         // Initialize the map.
         map = googleMap;
 
@@ -117,7 +117,6 @@ public class EarthMapFragment extends Fragment implements OnMapReadyCallback {
         map.getUiSettings().setZoomControlsEnabled(true);
 
         addMapStyle(map);
-        addDefaultMarkerToMap();
     }
 
     private void addMapStyle(GoogleMap googleMap) {
@@ -131,45 +130,46 @@ public class EarthMapFragment extends Fragment implements OnMapReadyCallback {
         }
     }
 
-    private void addDefaultMarkerToMap() {
-        LatLng latLng = new LatLng(37.4220595,-122.0849086);
-        Marker marker = map.addMarker(new MarkerOptions().position(latLng));
-        marker.setDraggable(true);
+    private void addMarkerToMap(double lat, double lng) {
+        LatLng latLng = new LatLng(lat,lng);
+
+        // Note: By default, a marker is not draggable, it must be set to be draggable.
+        map.addMarker(new MarkerOptions().position(latLng).draggable(true));
 
         // Move the camera instantly to default marker with a zoom of 15.
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+
+        // Listen for drag events on the marker.
+        map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {}
+
+            @Override
+            public void onMarkerDrag(Marker marker) {}
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                LatLng position = marker.getPosition();
+                markerLat = position.latitude;
+                markerLng = position.longitude;
+                Timber.d("onMarkerDragEnd: " + markerLat + ", " + markerLng);
+                showSnackbar("Marker coordinates:\n " + markerLat + ", " + markerLng);
+            }
+        });
     }
 
-    private boolean checkReady() {
-        if (map == null) {
-            Toast.makeText(getHostActivity(), R.string.map_not_ready, Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        return true;
-    }
-
-    public void onClearMap() {
-        if (!checkReady()) return;
-        map.clear();
-    }
-
-    /**
-     * Provides a simple way of getting a device's location and is well suited for
-     * applications that do not require a fine-grained location and that do not need location
-     * updates. Gets the best and most recent location currently available, which may be null
-     * in rare cases when a location is not available.
-     * <p>
-     * Note: this method should be called after location permission has been granted.
-     */
     @SuppressWarnings("MissingPermission")
     private void getLastLocation() {
         fusedLocationClient.getLastLocation()
                 .addOnCompleteListener(getHostActivity(), task -> {
                     if (task.isSuccessful() && task.getResult() != null) {
                         lastLocation = task.getResult();
-                        Timber.d(lastLocation.getLatitude() + " " + lastLocation.getLongitude());
-                        showSnackbar(lastLocation.getLatitude() + " " + lastLocation.getLongitude());
+                        Timber.d("Last known location: " + lastLocation.getLatitude()
+                                + ", " + lastLocation.getLongitude());
+                        showSnackbar("Last known location:\n " + lastLocation.getLatitude()
+                                + ", " + lastLocation.getLongitude());
+                        addMarkerToMap(lastLocation.getLatitude(), lastLocation.getLongitude());
                     } else {
                         Timber.w("getLastLocation:exception: %s", task.getException());
                         showSnackbar(getString(R.string.no_location_detected));
@@ -179,19 +179,10 @@ public class EarthMapFragment extends Fragment implements OnMapReadyCallback {
                         Timber.w("getLastLocation:exception: %s", exception));
     }
 
-    private void showSnackbar(final String text) {
-        View container = getHostActivity().findViewById(R.id.main_root_layout);
-        if (container != null) {
-            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
-        }
-    }
-
-    private void showSnackbar(final int mainTextStringId, final int actionStringId,
-                              View.OnClickListener listener) {
-        Snackbar.make(getHostActivity().findViewById(android.R.id.content),
-                      getString(mainTextStringId),
-                      Snackbar.LENGTH_INDEFINITE)
-                .setAction(getString(actionStringId), listener).show();
+    @Override
+    public void onLocationChanged(Location location) {
+        if (map != null) map.clear();
+        addMarkerToMap(location.getLatitude(), location.getLongitude());
     }
 
     private boolean checkPermissions() {
@@ -207,31 +198,26 @@ public class EarthMapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     private void requestPermissions() {
-        boolean shouldProvideRationale =
-                ActivityCompat.shouldShowRequestPermissionRationale(getHostActivity(),
-                        Manifest.permission.ACCESS_COARSE_LOCATION);
+        boolean shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+                getHostActivity(), Manifest.permission.ACCESS_COARSE_LOCATION);
 
         // Provide an additional rationale to the user. This would happen if the user
         // denied the request previously, but didn't check the "Don't ask again" checkbox.
         if (shouldProvideRationale) {
             Timber.i("Displaying permission rationale to provide additional context.");
-            showSnackbar(R.string.permission_rationale, android.R.string.ok, view -> {
-                // Request permission
-                startLocationPermissionRequest();
-            });
+            showSnackbar(R.string.permission_rationale, android.R.string.ok, view ->
+                    startLocationPermissionRequest());
 
         } else {
-            Timber.i("Requesting permission");
-            // Request permission. It's possible this can be auto answered if device policy
-            // sets the permission in a given state or the user denied the permission
-            // previously and checked "Never ask again".
+            // Request permission. It's possible this can be auto answered if device
+            // policy sets the permission in a given state or the user denied the
+            // permission previously and checked "Never ask again".
+            Timber.i("Requesting permission.");
             startLocationPermissionRequest();
         }
     }
 
-    /**
-     * Callback received when a permissions request has been completed.
-     */
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
@@ -247,9 +233,9 @@ public class EarthMapFragment extends Fragment implements OnMapReadyCallback {
             } else {
                 // Permission denied.
 
-                // Notify the user via a SnackBar that they have rejected a core permission for the
-                // app, which makes the Activity useless. In a real app, core permissions would
-                // typically be best requested during a welcome-screen flow.
+                // Notify the user via a SnackBar that they have rejected a core permission
+                // for the app, which makes the Activity useless. In a real app, core permissions
+                // would typically be best requested during a welcome-screen flow.
 
                 // Additionally, it is important to remember that a permission might have been
                 // rejected without asking the user for permission (device policy or "Never ask
@@ -276,11 +262,24 @@ public class EarthMapFragment extends Fragment implements OnMapReadyCallback {
                 R.string.earth_map_explanation, Snackbar.LENGTH_INDEFINITE);
         snackbar.setAction(R.string.ok, view -> snackbar.dismiss());
         snackbar.show();
+    }
 
+    private void showSnackbar(final String text) {
+        View container = getHostActivity().findViewById(R.id.main_root_layout);
+        if (container != null) {
+            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void showSnackbar(final int textStringId, final int actionStringId,
+                              View.OnClickListener listener) {
+        Snackbar.make(getHostActivity().findViewById(android.R.id.content),
+                      getString(textStringId), Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(actionStringId), listener)
+                .show();
     }
 
     private MainActivity getHostActivity(){
         return (MainActivity) getActivity();
     }
-
 }
